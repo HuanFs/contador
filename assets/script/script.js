98c7dcc4c4c592394fe9fb7f2792d0b1b70142b3
Ruan Figuereido <huanfiguereido@gmail.com>
2023-04-15 13:27:02 -0300

    style
    ajuste de layout, mudança do papel de parede em diferentes telas

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,25 @@
-let display = document.querySelector("input");
-let btnMenos = document.getElementById("menos");
-let btnMais = document.getElementById("mais");
-let foto = document.querySelector("img");
+let display = document.querySelector("input");//Visor
+let btnMenos = document.getElementById("menos");//Botão subtrair
+let btnMais = document.getElementById("mais");//Botão Somar
+let foto = document.querySelector("img");//Imagem
 btnMenos.addEventListener("click", subtrair);
 btnMais.addEventListener("click", somar);
-let contagem = 0;
+let contagem = 0;//Contador
+//  FUNÇÃO SOMA
 function somar(){
     contagem++;
     display.value=contagem;
+    //SE MAIOR QUE 0 E MENOR QUE 10, ADICIONA DIGITO 0 NA FRENTE
     if(display.value<10&&display.value>0){
         display.value='0'+contagem;
     }
-    if(display.value>0||display.value<5){
+    //SE O VALOR FOR MENOR QUE 0 A IMAGEM NÃO APAREÇE
+    if(display.value<=(-1)){
+        foto.style.display="none";
+        foto.src="";
+    }
+    //SE MAIOR QUE ZERO E MENOR QUE 5, EXIBA A IMAGEM
+    if(display.value>0||display.value<6){
         foto.style.display="block";
     }  
     if(display.value==0){
@@ -29,15 +37,25 @@ function somar(){
     if(display.value==4){
         foto.src="assets/img/dog4.png";
     } 
+    if(display.value==5){
+        foto.src="assets/img/dog5.png";
+    } 
 }
-
+//  FUNÇÃO SUBTRAIR
 function subtrair(){
     contagem--;
     display.value=contagem;
+    //SE MAIOR QUE 0 E MENOR QUE 10, ADICIONA DIGITO 0 NA FRENTE
     if(display.value<10&&display.value>0){
         display.value='0'+contagem;
     }
-    if(display.value>0||display.value<5){
+    //SE O VALOR FOR MENOR QUE 0 A IMAGEM NÃO APAREÇE
+    if(display.value<=(-1)){
+        foto.style.display="none";
+        foto.src="";
+    }
+    //SE MAIOR QUE ZERO E MENOR QUE 5, EXIBA A IMAGEM
+    if(display.value>0||display.value<6){
         foto.style.display="block";
     } 
     if(display.value==0){
@@ -55,5 +73,20 @@ function subtrair(){
     if(display.value==4){
         foto.src="assets/img/dog4.png";
     } 
+    if(display.value==5){
+        foto.src="assets/img/dog5.png";
+    } 
+}
+
+function tela(){
+    let altura = window.innerHeight;
+    if(altura==667){
+        let conteudo = document.getElementById("container");
+        conteudo.style.marginTop="150px";
+    }
+    else if(altura>=700){
+        let conteudo = document.getElementById("container");
+        conteudo.style.marginTop="-50px";
+    }
 }
 
